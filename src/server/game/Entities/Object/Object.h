8ce083b33bf06d490f43f02653605f0a1a65bb1c
darkmajesty99 <jbln2005@hotmail.com>
2026-08-29 07:15:19 +0800

    Add ElunaEventProcessor support to WorldObject

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -41,6 +41,9 @@ class Corpse;
 class Creature;
 class CreatureAI;
 class DynamicObject;
+#ifdef ELUNA
+class ElunaEventProcessor;
+#endif
 class GameObject;
 class InstanceScript;
 class Map;
@@ -285,7 +288,11 @@ class FC_GAME_API WorldObject : public Object, public WorldLocation
     public:
         virtual ~WorldObject();
 
+#ifdef ELUNA
+        virtual void Update (uint32 /*time_diff*/);
+#else
         virtual void Update (uint32 /*time_diff*/) { }
+#endif
 
         void _Create(ObjectGuid::LowType guidlow, HighGuid guidhigh);
         void AddToWorld() override;
@@ -472,6 +479,10 @@ class FC_GAME_API WorldObject : public Object, public WorldLocation
         bool IsWorldObject() const;
 
         uint32  LastUsedScriptID;
+
+#ifdef ELUNA
+        ElunaEventProcessor* elunaEvents;
+#endif
 
         // Transports
         TransportBase* GetTransport() const { return m_transport; }
